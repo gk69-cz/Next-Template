719fa4d9e8a34f213606bb14de6784b348dc584f
Christy Zachariah <zachariahchristy@gmail.com>
2024-04-28 23:18:03 +0530

    document

---
+++ app/_document.js
@@ -0,0 +1,6 @@
+<Head>
+    <script async src="https://pagead2.googlesyndication.com/pagead/js/adsbygoogle.js?client=ca-pub-4485865253703650"
+        crossorigin="anonymous">
+
+    </script>
+</Head>
